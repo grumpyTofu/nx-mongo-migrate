ea794628699a32e3db469e1a3320e8b242d50f50
grumpytofu <>
2023-02-01 22:27:05 -0500

    add ci/cd publish

---
+++ tools/scripts/publish.ts
@@ -0,0 +1,43 @@
+import { readCachedProjectGraph } from '@nrwl/devkit';
+import { execSync } from 'child_process';
+import * as fs from 'fs';
+import * as path from 'path';
+import chalk from 'chalk';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+const main = async () => {
+  const { name } = await yargs(hideBin(process.argv))
+    .option('name', {
+      alias: 'n',
+      description: 'The name of the project being deployed',
+      type: 'string',
+      demandOption: true,
+    })
+    .parse();
+
+  const rootPath = process.cwd();
+
+  const graph = readCachedProjectGraph();
+  const project = graph.nodes[name];
+
+  if (!project)
+    throw `Could not find project "${name}" in the workspace. Is the project.json configured correctly?`;
+
+  const outputPath = path.join(
+    rootPath,
+    project.data?.targets?.build?.options?.outputPath
+  );
+  if (!outputPath)
+    throw `Could not find "build.options.outputPath" of project "${name}". Is project.json configured correctly?`;
+
+  const config = '.npmrc';
+  fs.cpSync(config, path.join(outputPath, config));
+  process.chdir(outputPath);
+  //execSync('yarn publish');
+};
+
+main().catch((error) => {
+  console.error(chalk.bold.red('An error occurred during deployment', error));
+  throw error;
+});
