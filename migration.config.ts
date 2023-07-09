f02d30b769a5685d62b877ba826a2fdb6e711725
grumpytofu <>
2023-07-08 21:43:59 -0400

    cleanup migration config for example

--- migration.config.ts
+++ migration.config.ts
@@ -1,24 +1,9 @@
-import mongoose from 'mongoose';
-export interface MigrationConfig extends mongoose.ConnectOptions {
-  dbUri: string;
-  migrationCollection: string;
-}
-
-export type MigrationConfigFactory = () =>
-  | MigrationConfig
-  | Promise<MigrationConfig>;
-
-/* IMPORTANT NOTE: 
-Your config will look slightly different
-Your repo should be able to import MigrationConfigFactory type directly from nx-mongo-migrate
-I am faking the type for demo purposes below
-This config works locally with the example package in this repo for testing purposes
-*/
+import { MigrationConfigFactory } from './packages/mongo-migrate';
 
 const config: MigrationConfigFactory = () => ({
   dbUri: 'mongodb://localhost:27017',
   migrationCollection: 'migrations', // default
-  dbName: 'nx-mongo-migrate'
+  dbName: 'nx-mongo-migrate',
 });
 
 export default config;
